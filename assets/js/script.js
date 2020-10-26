a6584076f2fed55d9eb045bafc54c781f6b83876
Florence Jacobs <florencejacobs85@gmail.com>
2020-10-26 11:18:07 +0100

    joannatoilamain

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
 (() => { 
 let score = 0;
+let scoreHTML = document.getElementById("score");
 
 function incrementScore (){
 score += 1;
+scoreHTML.innerHTML=score;
 }
+
 
     document.getElementById("clickMe").addEventListener("click", () => {
         incrementScore();
 
-    }
+    });
+
+    document.getElementById("part-1").addEventListener("click", () => {
+    
+    
+    });
+
 
 
 })();
